4d52a81011fd66d62aae04489a64438ce893df83
chenxingyong <2103095120@qq.com>
2025-07-12 20:22:36 +0800

    有序数组的平方

--- Base/src/dynamicArray/twoPointers/leetCode/LeetCode977.java
+++ Base/src/dynamicArray/twoPointers/leetCode/LeetCode977.java
@@ -6,7 +6,7 @@ public class LeetCode977 {
 
     public static void main(String[] args) {
         int [] num = {-4,-1,0,3,10};
-        System.out.println(Arrays.toString(sortedSquares(num)));
+        System.out.println(Arrays.toString(sortArrayDouble(num)));
     }
 
     /**
@@ -14,25 +14,21 @@ public class LeetCode977 {
      * 元素的平方最大值一定产生在原数组的最左边或者最右边
      * 我们将最大值放入平方数组的最后一个位置就好了。
      */
-    public static int[] sortedSquares(int[] nums){
+    public static int[] sortArrayDouble(int [] num){
+        int[] result = new int[num.length];
+        int right = num.length - 1;
         int left = 0;
-        int right = nums.length-1;
-        int[]result = new int[nums.length];
-
-        // 写入的最后一个位置
-        int writer = nums.length-1;
-
-        // 如左右指针相遇（逐渐靠拢的过程）之后不再循环
+        // 最后一个位置索引
+        int wait = num.length - 1;
         while (left <= right){
-            // 如果原数组的左指针对应的平方值大于右指针，那么往新数组最后位置写入左指针对应的平方值
-            if (nums[left]*nums[left] > nums[right] * nums[right]){
-                result[writer] = nums[left]*nums[left];
+            if (num[left] * num[left] > num[right] * num[right]){
+                result[wait] = num[left] * num[left];
+                wait--;
                 left++;
-                writer--;
             }else {
-                result[writer] = nums[right]*nums[right];
+                result[wait] = num[right] * num[right];
+                wait--;
                 right--;
-                writer--;
             }
         }
         return result;
