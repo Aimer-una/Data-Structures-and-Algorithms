04ee1d7d53e87ef5221b0cefdb3041ac337bc95b
chenxingyong <2103095120@qq.com>
2025-07-13 16:27:09 +0800

    链表 LeetCode203（移除链表元素）方法二：原链表删除元素

--- Base/src/linkList/leetcode/LeetCode203.java
+++ Base/src/linkList/leetcode/LeetCode203.java
@@ -4,7 +4,8 @@ import linkList.ListNode;
 
 public class LeetCode203 {
 
-    public ListNode removeElements(ListNode head, int val) {
+    // 方法一：定义虚拟头节点
+    public ListNode removeElementsMethod(ListNode head, int val) {
         // 设置一个虚拟的头结点
         ListNode dummy = new ListNode();
         dummy.next = head;
@@ -18,5 +19,21 @@ public class LeetCode203 {
             }
         }
         return dummy.next;
+    }
+
+    // 方法二：原链表删除元素
+    public ListNode removeElements2(ListNode head, int val){
+        while (head != null && head.val == val){
+            head = head.next;
+        }
+        ListNode cur = head;
+        while (cur != null && cur.next != null){
+            if (cur.next.val == val){
+                cur.next = cur.next.next;
+            }else {
+                cur = cur.next;
+            }
+        }
+        return head;
     }
 }
