1d4810a1234654a99a9656f08e070012410d941e
chenxingyong <2103095120@qq.com>
2025-07-15 19:17:22 +0800

    LeetCode206反转链表(递归)

--- Base/src/linkList/leetcode/LeetCode206.java
+++ Base/src/linkList/leetcode/LeetCode206.java
@@ -3,6 +3,10 @@ package linkList.leetcode;
 import linkList.ListNode;
 
 public class LeetCode206 {
+    public static void main(String[] args) {
+        ListNode head = new ListNode();
+        reverseRecursion(head,null);
+    }
 
     // 反转链表(双指针方法)
     public ListNode reverseListNode(ListNode head){
@@ -18,5 +22,15 @@ public class LeetCode206 {
             cur = temp;
         }
         return pre;
+    }
+
+    // 反转链表(递归)
+    public static ListNode reverseRecursion(ListNode cur , ListNode pre){
+        if (cur == null) return pre;
+        // 定义一个临时指针，来保存cur的下一个节点
+        ListNode temp = null;
+        temp = cur.next;
+        cur.next = pre;
+       return reverseRecursion(temp,cur);
     }
 }
