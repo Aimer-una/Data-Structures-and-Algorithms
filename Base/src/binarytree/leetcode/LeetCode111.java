3645e70970ccb86d86f036af9ae917c794accb78
chenxingyong <2103095120@qq.com>
2025-08-07 21:39:04 +0800

    使用排序搜索 求最小深度

--- Base/src/binarytree/leetcode/LeetCode111.java
+++ Base/src/binarytree/leetcode/LeetCode111.java
@@ -2,6 +2,9 @@ package binarytree.leetcode;
 
 import binarytree.TreeNode;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 // 二叉树最小深度
 public class LeetCode111 {
     public int minDepth(TreeNode root) {
@@ -17,5 +20,33 @@ public class LeetCode111 {
             return leftSize+1;
         }
         return Math.min(rightSize,leftSize)+1;
+    }
+
+    // 层序排列方法
+    public int minDepthOrder(TreeNode root) {
+        if (root == null){
+            return 0;
+        }
+        Deque<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int size = 0;
+        while (!queue.isEmpty()){
+            int len = queue.size();
+            size = size+1;
+            while (len > 0){
+                TreeNode node = queue.poll();
+                if (node.right == null && node.left == null){
+                    return size;
+                }
+                if (node.left != null){
+                    queue.offer(node.left);
+                }
+                if (node.right != null){
+                    queue.offer(node.right);
+                }
+                len--;
+            }
+        }
+        return size;
     }
 }
