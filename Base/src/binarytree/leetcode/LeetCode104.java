0135362496549385443e852b1ddde62affec4511
chenxingyong <2103095120@qq.com>
2025-08-07 19:23:28 +0800

    使用排序搜索 求最大深度

--- Base/src/binarytree/leetcode/LeetCode104.java
+++ Base/src/binarytree/leetcode/LeetCode104.java
@@ -2,7 +2,12 @@ package binarytree.leetcode;
 
 import binarytree.TreeNode;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
+// 求二叉树最大深度
 public class LeetCode104 {
+    // 递归
     public int maxDepth(TreeNode root) {
         if (root == null){
             return 0;
@@ -10,6 +15,31 @@ public class LeetCode104 {
         int leftSize = maxDepth(root.left);
         int rightSize = maxDepth(root.right);
         int max = 1 + Math.max(leftSize,rightSize);
+        return max;
+    }
+
+    // 层序搜索
+    public int maxDepthOrder(TreeNode root) {
+        if (root == null){
+            return 0;
+        }
+        Deque<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int max = 0;
+        while (!queue.isEmpty()){
+            int len = queue.size();
+            max += 1;
+            while (len > 0){
+                TreeNode node = queue.pop();
+                if (node.left != null){
+                    queue.offer(node.left);
+                }
+                if (node.right != null){
+                    queue.offer(node.right);
+                }
+                len--;
+            }
+        }
         return max;
     }
 }
