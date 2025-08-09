84990ed7ffad8b59c539407dd4dd2678db1f0463
chenxingyong <2103095120@qq.com>
2025-08-09 23:36:11 +0800

    LeetCode222 搜索完全二叉树中的节点数量(使用层序遍历发解决)

--- Base/src/binarytree/leetcode/LeetCode222.java
+++ Base/src/binarytree/leetcode/LeetCode222.java
@@ -2,6 +2,9 @@ package binarytree.leetcode;
 
 import binarytree.TreeNode;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 // 获取完全二叉树的所有节点数量
 public class LeetCode222 {
     // 利用满二叉树方法实现
@@ -26,5 +29,30 @@ public class LeetCode222 {
         int left = countNodes(root.left);
         int right = countNodes(root.right);
         return left+right+1;
+    }
+
+    // 层序遍历法
+    public int countNodesOrder(TreeNode root) {
+        if (root == null){
+            return 0;
+        }
+        Deque<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int size = 0;
+        while (!queue.isEmpty()){
+            int len = queue.size();
+            while (len > 0){
+                size++;
+                TreeNode node = queue.poll();
+                if (node.left != null){
+                    queue.offer(node.left);
+                }
+                if (node.right != null){
+                    queue.offer(node.left);
+                }
+                len--;
+            }
+        }
+        return size;
     }
 }
