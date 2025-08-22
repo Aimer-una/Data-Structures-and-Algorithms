0a687cb3d427b7f74e4689cb17b4b8d3bac45cb5
chenxingyong <2103095120@qq.com>
2025-08-22 19:56:15 +0800

    236. 二叉树的最近公共祖先

---
+++ Base/src/binarytree/leetcode/LeetCode236.java
@@ -0,0 +1,25 @@
+package binarytree.leetcode;
+
+import binarytree.TreeNode;
+
+public class LeetCode236 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q){
+            return root;
+        }
+        // 左
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        // 右
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        // 中
+        if (left == null && right == null){
+            return null;
+        }else if (left != null && right == null){
+            return left;
+        }else if (right != null && left == null){
+            return right;
+        }else {
+            return root;
+        }
+    }
+}
