3b38c431f6c4ed6c66d1e0b126d3d5d8fc90536f
chenxingyong <2103095120@qq.com>
2025-08-25 15:26:56 +0800

    冒泡排序

---
+++ Base/src/sorting/BubbleSort.java
@@ -0,0 +1,32 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void bubbleSort(int[]nums){
+        if (nums == null || nums.length <= 1){
+            return;
+        }
+        int length = nums.length;
+        for (int i = 0; i < length-1; i++) {
+            boolean swapped = false;
+            for (int j = 0; j < length-1-i; j++) {
+                if (nums[j] > nums[j+1]){
+                    int temp = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped){
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[]nums = {5,4,3,2,1};
+        bubbleSort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
