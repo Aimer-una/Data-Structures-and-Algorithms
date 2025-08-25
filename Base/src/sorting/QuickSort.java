725d0a3471db004db076e958c6f0df673c58d3fc
chenxingyong <2103095120@qq.com>
2025-08-25 17:52:36 +0800

    快速排序

---
+++ Base/src/sorting/QuickSort.java
@@ -0,0 +1,36 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class QuickSort {
+    public static void quickSort(int[]nums,int low,int high){
+        if (low < high){
+            int partition = partition(nums, low, high);
+            // 左边
+            quickSort(nums,low,partition-1);
+            // 右边
+            quickSort(nums,partition+1,high);
+        }
+    }
+    public static int partition(int[]nums,int low,int high){
+        int mid = nums[low];
+        while (low < high){
+            while (low < high && nums[high] >= mid){
+                high--;
+            }
+            nums[low] = nums[high];
+            while (low < high && nums[low] <= mid){
+                low++;
+            }
+            nums[high] = nums[low];
+        }
+        nums[low] = mid;
+        return low;
+    }
+
+    public static void main(String[] args) {
+        int[]nums = {5,4,3,2,1};
+        quickSort(nums,0,nums.length-1);
+        System.out.println(Arrays.toString(nums));
+    }
+}
