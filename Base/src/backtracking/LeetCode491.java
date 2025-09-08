5cf0edc19694f7dc1a83937fd503995820fb42a3
chenxingyong <2103095120@qq.com>
2025-09-08 20:39:06 +0800

    90. 子集 II

---
+++ Base/src/backtracking/LeetCode491.java
@@ -0,0 +1,31 @@
+package backtracking;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+// 491. 非递减子序列
+public class LeetCode491 {
+    List<List<Integer>> result = new ArrayList<>();
+    List<Integer> path = new ArrayList<>();
+    public List<List<Integer>> findSubsequences(int[] nums) {
+        backtracking(nums,0);
+        return result;
+    }
+
+    public void backtracking(int[] nums, int startIndex){
+        if (path.size() > 1){
+            result.add(new ArrayList<>(path));
+        }
+        Set<Integer> set = new HashSet<>();
+        for (int i = startIndex; i < nums.length; i++) {
+            if (!path.isEmpty() && path.get(path.size() - 1) > nums[i] || set.contains(nums[i])){
+                continue;
+            }
+            path.add(nums[i]);
+            set.add(nums[i]);
+            backtracking(nums,i+1);
+            path.remove(path.size() - 1);
+        }
+    }
+}
