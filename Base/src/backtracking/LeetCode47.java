94770c5bc3299f86742ad97b16e5a925fd24be37
chenxingyong <2103095120@qq.com>
2025-09-10 20:40:16 +0800

    47. 全排列 II

---
+++ Base/src/backtracking/LeetCode47.java
@@ -0,0 +1,37 @@
+package backtracking;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+// 47. 全排列 II
+public class LeetCode47 {
+    List<List<Integer>> result = new ArrayList<>();
+    List<Integer> path = new ArrayList<>();
+    boolean[]used;
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        Arrays.sort(nums);
+        used = new boolean[nums.length];
+        backtracking(nums,used);
+        return result;
+    }
+
+    public void backtracking(int[] nums, boolean[] used){
+        if (path.size() == nums.length){
+            result.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if ( i > 0 && nums[i] == nums[i-1] && !used[i-1]){
+                continue;
+            }
+            if (used[i]){
+                continue;
+            }
+            path.add(nums[i]);
+            used[i] = true;
+            backtracking(nums,used);
+            path.remove(path.size()-1);
+            used[i] = false;
+        }
+    }
+}
