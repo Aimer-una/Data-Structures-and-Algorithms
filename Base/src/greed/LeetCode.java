a97d97f01e60e307937ef1d2dc54b0a57dc6836b
chenxingyong <2103095120@qq.com>
2025-09-16 21:19:58 +0800

    53. 最大子数组和

---
+++ Base/src/greed/LeetCode.java
@@ -0,0 +1,21 @@
+package greed;
+// 53. 最大子数组和
+public class LeetCode {
+    public int maxSubArray(int[] nums) {
+        if (nums.length == 1){
+            return nums[0];
+        }
+        int result = Integer.MIN_VALUE;
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            count += nums[i];
+            if (count > result){
+                result = count;
+            }
+            if (count < 0){
+                count = 0;
+            }
+        }
+        return result;
+    }
+}
