ae9488b66756ec2957a5324e944b5ed0a5b52894
chenxingyong <2103095120@qq.com>
2025-09-24 11:53:39 +0800

    746. 使用最小花费爬楼梯(空间优化：滚动变量)

--- Base/src/dynamicprograming/LeetCode746.java
+++ Base/src/dynamicprograming/LeetCode746.java
@@ -11,4 +11,21 @@ public class LeetCode746 {
         }
         return dp[n];
     }
+    // 空间优化：滚动变量
+    public int minCostClimbingStairs2(int[] cost) {
+        int n = cost.length;
+        if (n <= 1){
+            return 0;
+        }
+        int prev1 = 0;
+        int prev2 = 0;
+        int sum = 0;
+        for (int i = 2; i <= n ; i++) {
+           sum =  Math.min(prev1 + cost[i-1], prev2 + cost[i-2]);
+            prev2 = prev1;
+            prev1 = sum;
+        }
+        return sum;
+    }
+
 }
