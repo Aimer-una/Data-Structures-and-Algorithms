5e9e691c39b64817cea491ec2a1309731aa80561
chenxingyong <2103095120@qq.com>
2025-09-30 20:58:31 +0800

    167. 两数之和 II - 输入有序数组

---
+++ Base/src/灵茶山艾府/两数之和II输入有序数组.java
@@ -0,0 +1,19 @@
+package 灵茶山艾府;
+
+
+public class 两数之和II输入有序数组 {
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+        while (left < right){
+            if (numbers[left] + numbers[right] > target){
+                right--;
+            }else if (numbers[left] + numbers[right] < target){
+                left++;
+            }else {
+               return new int[]{left+1,right+1};
+            }
+        }
+        return new int[]{-1,-1};
+    }
+}
