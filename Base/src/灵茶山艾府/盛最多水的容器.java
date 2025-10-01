f388179875e71cf2bb75c16bbb47754e134d9dd8
chenxingyong <2103095120@qq.com>
2025-10-01 10:38:26 +0800

    盛最多水的容器

---
+++ Base/src/灵茶山艾府/盛最多水的容器.java
@@ -0,0 +1,19 @@
+package 灵茶山艾府;
+
+public class 盛最多水的容器 {
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int ans = 0;
+        while (left < right){
+            int area = (right - left) * Math.min(height[left],height[right]);
+            ans = Math.max(area,ans);
+            if (height[left] < height[right]){
+                left++;
+            }else {
+                right--;
+            }
+        }
+        return ans;
+    }
+}
