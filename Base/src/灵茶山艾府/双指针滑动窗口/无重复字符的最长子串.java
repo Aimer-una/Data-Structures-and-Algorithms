4402b23ba52c33712bf9d9cd0766087da8e8242a
chenxingyong <2103095120@qq.com>
2025-10-01 17:49:47 +0800

    无重复字符的最长子串

---
+++ Base/src/灵茶山艾府/双指针滑动窗口/无重复字符的最长子串.java
@@ -0,0 +1,25 @@
+package 灵茶山艾府.双指针滑动窗口;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class 无重复字符的最长子串 {
+    public int lengthOfLongestSubstring(String s) {
+        if (s == null || s.isEmpty()){
+            return 0;
+        }
+        int left = 0;
+        int ans = 0;
+        Map<Character,Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            // 如果 c 已经在当前窗口中出现过（注意：要 >= left）
+            if (map.containsKey(c) && map.get(c) >= left){
+                left = map.get(c) + 1;
+            }
+            map.put(c,i);
+            ans = Math.max(ans,i - left + 1);
+        }
+        return ans;
+    }
+}
