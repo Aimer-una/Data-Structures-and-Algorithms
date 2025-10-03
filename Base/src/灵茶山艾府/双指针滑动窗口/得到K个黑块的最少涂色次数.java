785c01e781c122529aa7f8748729838780c140a5
chenxingyong <2103095120@qq.com>
2025-10-03 21:36:37 +0800

    得到K个黑块的最少涂色次数

---
+++ Base/src/灵茶山艾府/双指针滑动窗口/得到K个黑块的最少涂色次数.java
@@ -0,0 +1,26 @@
+package 灵茶山艾府.双指针滑动窗口;
+
+public class 得到K个黑块的最少涂色次数 {
+    public static void main(String[] args) {
+        System.out.println(minimumRecolors("WBWW", 2));
+    }
+    public static int minimumRecolors(String blocks, int k) {
+        int sum = 0;
+        int result = Integer.MAX_VALUE;
+        for (int i = 0; i < blocks.length(); i++) {
+            char c = blocks.charAt(i);
+            if (c == 'W'){
+                sum++;
+            }
+            int left = i-k+1;
+            if (left < 0 ){
+                continue;
+            }
+            result = Math.min(result,sum);
+            if (blocks.charAt(left) == 'W'){
+                sum--;
+            }
+        }
+        return  result;
+    }
+}
