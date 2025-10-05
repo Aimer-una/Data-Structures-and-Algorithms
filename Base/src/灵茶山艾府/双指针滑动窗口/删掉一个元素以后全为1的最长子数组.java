fae051046c7f0077a3890dc54fd5a4ed0d16fc4c
chenxingyong <2103095120@qq.com>
2025-10-05 23:04:20 +0800

    删掉一个元素以后全为 1 的最长子数组

---
+++ Base/src/灵茶山艾府/双指针滑动窗口/删掉一个元素以后全为1的最长子数组.java
@@ -0,0 +1,19 @@
+package 灵茶山艾府.双指针滑动窗口;
+
+public class 删掉一个元素以后全为1的最长子数组 {
+    public int longestSubarray(int[] nums) {
+        int left = 0;
+        int result = 0;
+        int cnt0 = 0;
+        for (int right = 0; right < nums.length; right++) {
+            cnt0 += 1 - nums[right];
+            while (cnt0 > 1){
+                cnt0 -= 1 - nums[left];
+                left++;
+            }
+            // 至少要减去一个
+            result = Math.max(result,right-left);
+        }
+        return result;
+    }
+}
