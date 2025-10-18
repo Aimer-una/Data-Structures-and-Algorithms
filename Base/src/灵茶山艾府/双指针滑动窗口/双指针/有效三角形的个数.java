7bfd634337a94e417a5113badbf1c526839d1195
chenxingyong <2103095120@qq.com>
2025-10-18 11:26:12 +0800

    有效三角形的个数(同向双指针)

--- Base/src/灵茶山艾府/双指针滑动窗口/双指针/有效三角形的个数.java
+++ Base/src/灵茶山艾府/双指针滑动窗口/双指针/有效三角形的个数.java
@@ -3,7 +3,7 @@ package 灵茶山艾府.双指针滑动窗口.双指针;
 import java.util.Arrays;
 
 public class 有效三角形的个数 {
-    public  int triangleNumber(int[] nums) {
+    public int triangleNumber(int[] nums) {
         int n = nums.length;
         int ans = 0;
         Arrays.sort(nums);
@@ -16,6 +16,21 @@ public class 有效三角形的个数 {
                     right--;
                 }else {
                     left++;
+                }
+            }
+        }
+        return ans;
+    }
+
+    public int triangleNumber1(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int ans = 0;
+        for (int i = 0; i < n - 2; i++) {
+            int k = i + 2;
+            for (int j = i + 1; j < n - 1; j++) {
+                if (nums[i] + nums[j] > nums[k]){
+                    ans = k - j - 1;
                 }
             }
         }
