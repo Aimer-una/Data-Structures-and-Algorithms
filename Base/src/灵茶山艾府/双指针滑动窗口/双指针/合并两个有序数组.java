2a1a4951a723521d435280ec0d55d21458f69c74
chenxingyong <2103095120@qq.com>
2025-10-21 14:55:25 +0800

    合并两个有序数组

--- Base/src/灵茶山艾府/双指针滑动窗口/双指针/合并两个有序数组.java
+++ Base/src/灵茶山艾府/双指针滑动窗口/双指针/合并两个有序数组.java
@@ -6,11 +6,12 @@ public class 合并两个有序数组 {
         int p1 = m - 1;
         int p2 = n - 1;
         int p = n + m - 1;
-        while (p2 >= 0){
+        while (p2 >= 0){ // nums2 还有要合并的元素
+            // 如果 p1 < 0，那么走 else 分支，把 nums2 合并到 nums1 中
             if (p1 >= 0 && nums1[p1] > nums2[p2]){
-                nums1[p--] = nums1[p1--];
+                nums1[p--] = nums1[p1--]; // 填入 nums1[p1]
             }else {
-                nums1[p--] = nums2[p2--];
+                nums1[p--] = nums2[p2--]; // 填入 nums2[p1]
             }
         }
     }
