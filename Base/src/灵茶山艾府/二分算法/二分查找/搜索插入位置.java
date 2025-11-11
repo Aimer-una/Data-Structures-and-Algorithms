53a7148d2ae0862af4e8daafb5c47e186edde015
chenxingyong <2103095120@qq.com>
2025-11-11 08:59:27 +0800

    搜索插入位置

---
+++ Base/src/灵茶山艾府/二分算法/二分查找/搜索插入位置.java
@@ -0,0 +1,17 @@
+package 灵茶山艾府.二分算法.二分查找;
+
+public class 搜索插入位置 {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right){
+            int mid = left + right >>> 1;
+            if (nums[mid] >= target){
+                right = mid - 1;
+            }else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
