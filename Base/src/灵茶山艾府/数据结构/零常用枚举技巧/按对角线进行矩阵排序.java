b29ce79e7b225ae6fa75bccb6c88bd01bb6103a5
chenxingyong <2103095120@qq.com>
2025-11-27 17:35:19 +0800

    按对角线进行矩阵排序

--- Base/src/灵茶山艾府/数据结构/零常用枚举技巧/按对角线进行矩阵排序.java
+++ Base/src/灵茶山艾府/数据结构/零常用枚举技巧/按对角线进行矩阵排序.java
@@ -13,8 +13,8 @@ public class 按对角线进行矩阵排序 {
         // 令 k=i-j+n，那么右上角 k=1，左下角 k=m+n-1
         for (int k = 1; k < m + n; k++) {
             // 核心：计算 j 的最小值和最大值
-            int MinJ = Math.max(n-k,0); // i=0 的时候，j=n-k，但不能是负数
-            int MaxJ = Math.min(m+n-1-k,n-1); // i=m-1 的时候，j=m+n-1-k，但不能超过 n-1
+            int MinJ = Math.max(n-k,0); // i=0 的时候取最小值，j=n-k，但不能是负数
+            int MaxJ = Math.min(m+n-1-k,n-1); // i=m-1 的时候取最大值，j=m+n-1-k，但不能超过 n-1
             List<Integer> a = new ArrayList<>(MaxJ - MinJ + 1);
             for (int j = MinJ; j <= MaxJ; j++) {
                 a.add(grid[k-n+j][j]);
