a919504b4e1af0ca08d22f5b10db8928c33a6f33
chenxingyong <2103095120@qq.com>
2025-12-09 13:30:12 +0800

    统计特殊三元组（每日一题）

--- Base/src/每日一题/统计特殊三元组.java
+++ Base/src/每日一题/统计特殊三元组.java
@@ -1,25 +1,33 @@
-package 灵茶山艾府.数据结构.零常用枚举技巧;
+package 每日一题;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class 统计特殊三元组 {
+    /**
+     * 枚举中间
+     */
+
     public int specialTriplets(int[] nums) {
         final int MOD = 1_000_000_007;
+        long ans = 0;
         Map<Integer,Integer> suf = new HashMap<>();
+        // 统计j+1到n-1的数
         for (int num : nums) {
             suf.put(num,suf.getOrDefault(num,0)+1);
         }
-        long ans = 0;
+        // 统计j-1,j的数
         Map<Integer,Integer> pre = new HashMap<>();
         for (int num : nums) {
-            // 把当前的num从map中移除,因为j不在左右
+            // 先去除j
             suf.put(num,suf.getOrDefault(num,0)-1);
+            // 计算目标值
             int target = num * 2;
-            Integer rightCount = suf.getOrDefault(target, 0);
-            Integer leftCount = pre.getOrDefault(target, 0);
-            ans += (long) rightCount * leftCount;
-
+            // 找左右两边是否存在target,然后相乘
+            Integer right = suf.getOrDefault(target,0);
+            Integer left = pre.getOrDefault(target,0);
+            ans += (long)right * left;
+            // 将num加入到j-1的hash表中
             pre.put(num,pre.getOrDefault(num,0)+1);
         }
         return (int) (ans % MOD);
