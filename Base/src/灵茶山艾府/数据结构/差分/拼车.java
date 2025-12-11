7aaced2d93022e346b57093367edf208912b8051
chenxingyong <2103095120@qq.com>
2025-12-11 23:24:48 +0800

    拼车

---
+++ Base/src/灵茶山艾府/数据结构/差分/拼车.java
@@ -0,0 +1,22 @@
+package 灵茶山艾府.数据结构.差分;
+
+public class 拼车 {
+    public boolean carPooling(int[][] trips, int capacity) {
+        int[] cnt = new int[1002];
+        // 差分
+        for (int[] trip : trips) {
+            cnt[trip[1]] += trip[0];
+            cnt[trip[2]] -= trip[0];
+        }
+        // 前缀和
+        for (int i = 1; i <= cnt.length; i++) {
+            cnt[i] += cnt[i-1];
+        }
+        for (int i = 1; i < cnt.length; i++) {
+            if (cnt[i] > capacity){
+                return false;
+            }
+        }
+        return true;
+    }
+}
