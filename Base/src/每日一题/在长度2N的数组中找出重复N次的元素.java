1fde3aa7f88311b8689055db9d4c6685d3fc5283
chenxingyong <2103095120@qq.com>
2026-01-02 14:47:31 +0800

    在长度2N的数组中找出重复N次的元素

---
+++ Base/src/每日一题/在长度2N的数组中找出重复N次的元素.java
@@ -0,0 +1,22 @@
+package 每日一题;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class 在长度2N的数组中找出重复N次的元素 {
+    public int repeatedNTimes(int[] nums) {
+        int length = nums.length;
+        int n = length / 2;
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            Integer value = map.get(nums[i]);
+            if (value == n){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+}
