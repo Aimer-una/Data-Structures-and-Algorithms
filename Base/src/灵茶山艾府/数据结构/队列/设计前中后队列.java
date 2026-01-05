05628d1fa0dd77de4254e248b9db1dbf8e876b0e
chenxingyong <2103095120@qq.com>
2026-01-05 22:30:42 +0800

    设计前中后队列

---
+++ Base/src/灵茶山艾府/数据结构/队列/设计前中后队列.java
@@ -0,0 +1,67 @@
+package 灵茶山艾府.数据结构.队列;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class 设计前中后队列 {
+
+
+
+    private final Deque<Integer> left = new ArrayDeque<>();
+    private final Deque<Integer> right = new ArrayDeque<>();
+    // 调整长度，保证 0 <= right.size() - left.size() <= 1
+    // 从而保证可以在正中间插入删除元素
+    private void balance(){
+        if (left.size() > right.size()){
+            right.addFirst(left.pollLast());
+        }else if (right.size() > left.size() + 1){
+            left.addLast(right.pollFirst());
+        }
+    }
+
+    public void pushFront(int val) {
+        left.addFirst(val);
+        balance();
+    }
+
+    public void pushMiddle(int val) {
+        if (left.size() < right.size()){
+            left.addLast(val);
+        }else {
+            right.addFirst(val);
+        }
+    }
+
+    public void pushBack(int val) {
+        right.addLast(val);
+        balance();
+    }
+
+    public int popFront() {
+        if (right.isEmpty()){
+            return -1;
+        }
+        int val = left.isEmpty() ? right.pollFirst() : left.pollFirst();
+        balance();
+        return val;
+    }
+
+    public int popMiddle() {
+        if (right.isEmpty()){
+            return -1;
+        }
+        if (left.size() == right.size()){
+           return left.pollLast();
+        }
+        return right.pollFirst();
+    }
+
+    public int popBack() {
+        if (right.isEmpty()){
+            return -1;
+        }
+        int val = right.pollLast();
+        balance();
+        return val;
+    }
+}
