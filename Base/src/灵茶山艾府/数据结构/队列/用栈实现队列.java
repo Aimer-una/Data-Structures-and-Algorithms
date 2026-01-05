fc44ec9f5ff43979872cf0540a387ab09258e9c4
chenxingyong <2103095120@qq.com>
2026-01-05 23:04:01 +0800

    用栈实现队列

---
+++ Base/src/灵茶山艾府/数据结构/队列/用栈实现队列.java
@@ -0,0 +1,41 @@
+package 灵茶山艾府.数据结构.队列;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class 用栈实现队列 {
+    Deque<Integer> inStack;
+    Deque<Integer> outStack;
+    public 用栈实现队列() {
+        inStack = new ArrayDeque<>();
+        outStack = new ArrayDeque<>();
+    }
+
+    public void push(int x) {
+        inStack.push(x);
+    }
+
+    public int pop() {
+        if (outStack.isEmpty()){
+            in2out();
+        }
+        return outStack.pop();
+    }
+
+    public int peek() {
+        if (outStack.isEmpty()){
+            in2out();
+        }
+        return outStack.peek();
+    }
+
+    public boolean empty() {
+        return outStack.isEmpty() && inStack.isEmpty();
+    }
+
+    private void in2out(){
+        while (!inStack.isEmpty()){
+            outStack.push(inStack.pop());
+        }
+    }
+}
