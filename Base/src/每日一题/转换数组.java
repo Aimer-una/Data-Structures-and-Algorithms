3f54ea7676a9975feeb5e0519f09693d22d43d49
chenxingyong <2103095120@qq.com>
2026-02-05 10:29:36 +0800

    转换数组

---
+++ Base/src/每日一题/转换数组.java
@@ -0,0 +1,12 @@
+package 每日一题;
+
+public class 转换数组 {
+    public int[] constructTransformedArray(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+        for (int i = 0; i < n; i++) {
+            ans[i] = nums[((i+nums[i]) % n + n) % n];
+        }
+        return ans;
+    }
+}
