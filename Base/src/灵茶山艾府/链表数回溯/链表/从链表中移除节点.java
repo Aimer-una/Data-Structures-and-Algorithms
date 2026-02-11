5ee1ebbe808353a90215619ffc4972c2bd97fbd4
chenxingyong <2103095120@qq.com>
2026-02-11 20:10:34 +0800

    从链表中移除节点

---
+++ Base/src/灵茶山艾府/链表数回溯/链表/从链表中移除节点.java
@@ -0,0 +1,17 @@
+package 灵茶山艾府.链表数回溯.链表;
+
+import linkList.ListNode;
+
+public class 从链表中移除节点 {
+    public ListNode removeNodes(ListNode head) {
+        if (head.next == null){
+            return head;
+        }
+        ListNode node = removeNodes(head.next);
+        if (node.val > head.val){
+            return node;
+        }
+        head.next = node;
+        return head;
+    }
+}
