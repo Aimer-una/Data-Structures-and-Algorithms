e313b748fc67eeb6e7dde635026d408f0bb6950b
chenxingyong <2103095120@qq.com>
2026-02-15 21:20:56 +0800

    字母异位词分组

---
+++ Base/src/hot100/字母异位词分组.java
@@ -0,0 +1,19 @@
+package hot100;
+
+import java.util.*;
+
+public class 字母异位词分组 {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String,List> ans = new HashMap<>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String key = String.valueOf(chars);
+            if (!ans.containsKey(key)){
+                ans.put(key,new ArrayList<>());
+            }
+            ans.get(key).add(str);
+        }
+        return new ArrayList(ans.values());
+    }
+}
