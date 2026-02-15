d1b1f60eba0fa1550184707a997e058ee6986f30
chenxingyong <2103095120@qq.com>
2026-02-15 21:45:39 +0800

    字母异位词分组(方法二)

--- Base/src/hot100/字母异位词分组.java
+++ Base/src/hot100/字母异位词分组.java
@@ -3,8 +3,9 @@ package hot100;
 import java.util.*;
 
 public class 字母异位词分组 {
+    // 方法一
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String,List> ans = new HashMap<>();
+        Map<String,List<String>> ans = new HashMap<>();
         for (String str : strs) {
             char[] chars = str.toCharArray();
             Arrays.sort(chars);
@@ -16,4 +17,28 @@ public class 字母异位词分组 {
         }
         return new ArrayList(ans.values());
     }
+
+    // 方法二
+    public List<List<String>> groupAnagramsMethod2(String[] strs) {
+        Map<String,List<String>> map = new HashMap<>();
+        int[] count = new int[26];
+        for (String str : strs) {
+            Arrays.fill(count,0);
+            for (char c : str.toCharArray()) {
+                count[c - 'a']++;
+            }
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 26; i++) {
+                sb.append("#");
+                sb.append(count[i]);
+            }
+            String key = sb.toString();
+            if (!map.containsKey(key)){
+                map.put(key,new ArrayList<>());
+            }
+            map.get(key).add(str);
+        }
+        return new ArrayList(map.values());
+    }
+
 }
