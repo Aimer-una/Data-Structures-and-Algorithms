884d13bf4449778ed9f7a2b016e7f1c56b208464
chenxingyong <2103095120@qq.com>
2026-04-17 20:46:53 +0800

    搜索二维矩阵II(二分查找)

--- Base/src/hot100/搜索二维矩阵II.java
+++ Base/src/hot100/搜索二维矩阵II.java
@@ -12,4 +12,32 @@ public class 搜索二维矩阵II {
         }
         return false;
     }
+
+    // 二分查找
+    public boolean searchMatrixSearch(int[][] matrix, int target) {
+        for (int[] row : matrix) {
+            int isValid = search(row, target);
+            if (isValid >= 0){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int search(int[] arr,int target){
+        int high = arr.length - 1;
+        int low = 0;
+        while (low <= high){
+            int mid = (low + high) / 2;
+            int num = arr[mid];
+            if (num > target){
+                high = mid - 1;
+            }else if (num < target){
+                low = mid + 1;
+            }else {
+                return mid;
+            }
+        }
+        return -1;
+    }
 }
