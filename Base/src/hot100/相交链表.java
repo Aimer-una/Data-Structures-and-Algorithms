86dfaca82f36ab6a04f992d7d95c526c0e1f777a
chenxingyong <2103095120@qq.com>
2026-04-20 20:22:03 +0800

    相交链表

---
+++ Base/src/hot100/相交链表.java
@@ -0,0 +1,26 @@
+package hot100;
+
+import linkList.ListNode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class 相交链表 {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        Set<ListNode> set = new HashSet<>();
+        ListNode temp = headA;
+        while (temp != null){
+            set.add(temp);
+            temp = temp.next;
+        }
+
+        temp = headB;
+        while (temp != null){
+            if (set.contains(temp)){
+                return temp;
+            }
+            temp = temp.next;
+        }
+        return null;
+    }
+}
