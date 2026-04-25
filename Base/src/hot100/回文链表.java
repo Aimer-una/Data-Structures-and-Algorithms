aee09858f4c036591c55da4de17cfce17216cc10
chenxingyong <2103095120@qq.com>
2026-04-25 19:25:22 +0800

    迭代

--- Base/src/hot100/回文链表.java
+++ Base/src/hot100/回文链表.java
@@ -20,4 +20,41 @@ public class 回文链表 {
         left = left.next;
         return true;
     }
+
+    public boolean isPalindrome1(ListNode head) {
+        ListNode mid = middleNode(head);
+        ListNode head2 = reverseList(mid);
+        while (head2 != null){
+            if (head2.val != head.val){
+                return false;
+            }
+            head = head.next;
+            head2 = head2.next;
+        }
+        return true;
+    }
+
+    // 876. 链表的中间结点
+    private ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    // 206. 反转链表
+    private ListNode reverseList(ListNode head) {
+        ListNode cur = head;
+        ListNode pre = null;
+        while (cur != null){
+            ListNode temp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = temp;
+        }
+        return pre;
+    }
 }
