5d1f7b1cb66fe5db4c7038bc60d3a7deeab7b7d4
chenxingyong <2103095120@qq.com>
2026-05-04 18:22:37 +0800

    合并两个有序链表

---
+++ Base/src/hot100/合并两个有序链表.java
@@ -0,0 +1,23 @@
+package hot100;
+
+import linkList.ListNode;
+
+public class 合并两个有序链表 {
+    // 迭代
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode pre = dummy;
+        while (list1 != null && list2 != null){
+            if (list1.val <= list2.val){
+                pre.next = list1;
+                list1 = list1.next;
+            }else {
+                pre.next = list2;
+                list2 = list2.next;
+            }
+            pre = pre.next;
+        }
+        pre.next = list1 == null? list2 : list1;
+        return dummy.next;
+    }
+}
