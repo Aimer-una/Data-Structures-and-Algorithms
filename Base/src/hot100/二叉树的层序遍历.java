3238dc542ffa974167b1274363e658ec26e2a14a
chenxingyong <2103095120@qq.com>
2026-05-20 20:30:03 +0800

    二叉树的层序遍历(使用队列写法)

--- Base/src/hot100/二叉树的层序遍历.java
+++ Base/src/hot100/二叉树的层序遍历.java
@@ -3,7 +3,9 @@ package hot100;
 import binarytree.TreeNode;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class 二叉树的层序遍历 {
     //  两个数组的写法
@@ -28,6 +30,34 @@ public class 二叉树的层序遍历 {
             }
             cur = nxt;
             ans.add(vals);
+        }
+        return ans;
+    }
+
+    // 使用队列写法
+    public List<List<Integer>> levelOrderQueue(TreeNode root) {
+        if (root == null){
+            return List.of();
+        }
+        List<List<Integer>> ans = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()){
+            int n = queue.size();
+            // 预分配空间
+            List<Integer> list = new ArrayList<>(n);
+            while (n-- > 0){
+                TreeNode node = queue.poll();
+                if (node.left != null){
+                    queue.add(node.left);
+                }
+
+                if (node.right != null){
+                    queue.add(node.right);
+                }
+                list.add(node.val);
+            }
+            ans.add(list);
         }
         return ans;
     }
